547f736ca28ed99ea80cb051fd6da094d05f254e
Sam Merrick <samjmerrick@gmail.com>
2023-04-19 06:56:26 +0100

    Added particlebackground

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import { Inter } from "next/font/google";
 import Image from "next/image";
 import Avatar from "../assets/avatar.jpg";
+import ParticleBackground from "@/components/particlebackground";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center md:pt-32 pt-20 px-3 ">
-      <div className="bg-neutral-800 max-w-xl w-full flex flex-col items-start p-12 rounded-[2.5rem] space-y-5">
+      <div className="bg-neutral-800 max-w-xl w-full flex flex-col items-start p-12 rounded-[2.5rem] space-y-5 z-10">
         <Image
           src={Avatar}
           alt="Avatar"
@@ -26,6 +27,7 @@ export default function Home() {
           Get in touch
         </a>
       </div>
+      <ParticleBackground />
     </main>
   );
 }
