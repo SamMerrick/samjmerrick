64b198176e34b1ffc53780794c80068ecef7cdd1
Sam Merrick <samjmerrick@gmail.com>
2023-04-19 06:35:21 +0100

    Added basic card

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,21 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Sam Merrick - Freelance Developer</title>
+        <meta
+          name="description"
+          content="I transform ideas into captivating digital experiences that deliver
+          customer and commercial value"
+          key="desc"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
